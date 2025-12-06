cf8bb4922308d504f142329de4985943c9f11a34
arctic-shark <fahimshareear@gmail.com>
2025-12-07 03:04:20 +0600

    adding main body

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
+import { Suspense } from 'react';
 import './App.css'
 import Mainbody from './Componants/Mainbody/Mainbody';
 import Navbar from './Componants/navbar/Navbar';
 
-const userData = async () =>{
+
+const fetchData = async () =>{
   const res = await fetch('/Data.json');
   return res.json();
 };
 
-const userPromise = userData()
-console.log(userPromise);
-
-
+const userData = fetchData();
 
 function App() {
-  
 
   return (
     <>
       <Navbar></Navbar>
-      <Mainbody userPromise={userPromise}></Mainbody>
+      <Suspense fallback={<span className="loading loading-infinity loading-xl"></span>}>
+          <Mainbody userData={userData}></Mainbody>
+      </Suspense>
     </>
   )
 }
--- src/Componants/Mainbody/Mainbody.jsx
+++ src/Componants/Mainbody/Mainbody.jsx
@@ -1,12 +1,29 @@
 import React, { use } from 'react';
+// import Cards from '../Cards/Cards';
 
-const Mainbody = ({userPromise}) => {
-    const jsonData = use(userPromise);
-    console.log(jsonData)
+const Mainbody = ({userData}) => {
+    const jsonData = use(userData);
+    // console.log(jsonData);
+    // console.log(jsonData)
     return (
-        <div>
-            <div>
-                
+        <div className="md:w-9/12 mx-auto">
+            <div className="mx-auto flex items-center justify-between gap-2 m-3">
+                <div className="md:w-7/12 bg-[linear-gradient(#642fe3,#985cf0)] rounded-md flex items-center flex-col">
+                    <div className="text-center p-10">
+                        <h1 className="text-3xl">In-Progress</h1>
+                        <h1 className="text-6xl font-bold">{jsonData.length}</h1>
+                    </div>
+                </div>
+                <div className="md:w-7/12 bg-[linear-gradient(#54cf68,#0f8f77)] rounded-md flex items-center flex-col">
+                    <div className="text-center p-10">
+                        <h1 className="text-3xl">Resolved</h1>
+                        <h1 className="text-6xl font-bold">{jsonData.length}</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="border-2 border-green-600 flex justify-between items-center">
+                <h1 className="text-black font-bold text-2xl">Customers Tickets</h1>
+                <div>right</div>
             </div>
         </div>
     );
