8ff3ea48a6df02dbdcd2fb578e9270f3e1e146cb
arctic-shark <fahimshareear@gmail.com>
2025-12-06 19:17:43 +0600

    correcting background color

--- src/Body/Bodycard.jsx
+++ src/Body/Bodycard.jsx
@@ -4,7 +4,7 @@ import Card from './Card';
 const Bodycard = ({user}) => {
     console.log(user)
     return (
-        <div className="w-9/12 ">
+        <div className="w-8/12 ">
             <h1 className="text-[19px] font-bold text-black p-2">Customer Tickets</h1>
             <div className="md:grid md:grid-cols-2 gap-2 md:w-full sm:grid-cols-1 ml-4 shadow-md">
                 {
