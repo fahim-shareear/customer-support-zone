80070a3a7285016acc5c4048e948ae25ff0fb199
arctic-shark <fahimshareear@gmail.com>
2025-12-08 22:48:05 +0600

    ........

--- src/Componants/Navbar.jsx
+++ src/Componants/Navbar.jsx
@@ -50,12 +50,12 @@ const Navbar = ({ countProgress, resolvedCount }) => {
       <div className="top-50">
         <div className="md:max-w-[75%] mx-auto p-3 grid md:grid-cols-2 gap-3">
           
-          <div className="md:max-w-[600px] sm:max-w-full rounded-xl shadow-xl h-[260px] md:m-5 bg-linear-to-r from-purple-700 to-purple-600 text-center flex items-center flex-col gap-3 justify-center md:ml-15">
+          <div className="left md:max-w-[600px] sm:max-w-full rounded-xl shadow-xl h-[260px] md:m-5 bg-linear-to-r from-purple-900 to-purple-600 text-center flex items-center flex-col gap-3 justify-center md:ml-15">
             <h1 className="text-2xl">In Progress</h1>
             <h1 className="text-6xl">{countProgress}</h1>
           </div>
 
-          <div className="md:max-w-[600px] sm:w-full rounded-xl shadow-xl h-[260px] md:m-5 bg-linear-to-r from-green-400 to-green-700 text-center flex items-center flex-col gap-3 justify-center">
+          <div className="right md:max-w-[600px] sm:w-full rounded-xl shadow-xl h-[260px] md:m-5 bg-linear-to-r from-green-400 to-green-700 text-center flex items-center flex-col gap-3 justify-center">
             <h1 className="text-2xl">Resolved</h1>
             <h1 className="text-6xl">{resolvedCount}</h1>
           </div>
